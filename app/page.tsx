6db848a569a9c1209fe245e6480fa8b61c6adb81
Piotr Rudzki <rudzky@air-piotr.home>
2023-11-23 22:50:15 +0100

    :wrench: Create Initial Prisma Schema
    - Add initialProfile utility
    - Add db utility
    - Install @prisma/client

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,21 @@
-import { UserButton } from "@clerk/nextjs";
-import Image from "next/image";
+import { db } from "@/lib/db";
+import { initialProfile } from "@/lib/initialProfile";
 
-export default function Home() {
+export default async function Home() {
+  const user = await initialProfile();
+  const userWorkspaces = await db.workspace.findMany({
+    where: {
+      members: {
+        some: {
+          profileId: user.profileId,
+        },
+      },
+    },
+  });
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <UserButton afterSignOutUrl="/" />
+      Hello
     </main>
   );
 }
---
+++ lib/db.ts
@@ -0,0 +1,13 @@
+import { PrismaClient } from "@prisma/client";
+
+declare global {
+  var prisma: PrismaClient | undefined;
+}
+
+export const db =
+  globalThis.prisma ||
+  new PrismaClient({
+    errorFormat: "pretty",
+  });
+
+if (process.env.NODE_ENV !== "production") globalThis.prisma = db;
